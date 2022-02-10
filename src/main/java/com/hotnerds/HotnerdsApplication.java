8c2d4de0e9c24d9e4e4a5baa1b1c54195d1b346d
RIVERKIM <garamkim1357@gmail.com>
2022-02-10 19:58:00 +0900

    first commit

---
+++ src/main/java/com/hotnerds/HotnerdsApplication.java
@@ -0,0 +1,13 @@
+package com.hotnerds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotnerdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotnerdsApplication.class, args);
+	}
+
+}
