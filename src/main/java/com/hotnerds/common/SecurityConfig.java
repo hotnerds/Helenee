58b1a811af94e686415422ca4afefbf78e86b236
RIVERKIM <garamkim1357@gmail.com>
2022-02-12 14:44:23 +0900

    style:[폴더 순서 재배열] Config 설정을 최상위 common 파일로 뺏음.

--- src/main/java/com/hotnerds/common/SecurityConfig.java
+++ src/main/java/com/hotnerds/common/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.hotnerds.user.common;
+package com.hotnerds.common;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
