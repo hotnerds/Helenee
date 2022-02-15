82a1d3d9bac77d49c8f89d629d280bab66f95fe4
RetepMil <lkslyj2@naver.com>
2022-02-15 20:31:23 +0900

    refactor: findByUsername,findByEmail 반환 값을 Optional<User>로 변경

--- src/main/java/com/hotnerds/user/domain/repository/UserRepository.java
+++ src/main/java/com/hotnerds/user/domain/repository/UserRepository.java
@@ -4,10 +4,11 @@ import com.hotnerds.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    List<User> findByUsername(String username);
-    List<User> findByEmail(String username);
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String username);
 
 }
