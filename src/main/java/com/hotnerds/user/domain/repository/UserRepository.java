2586ef97f801858e18192eabd6c77b0cbb810b16
RetepMil <lkslyj2@naver.com>
2022-02-15 19:05:24 +0900

    feature: UserRepository에 findByUsername, findByEmail 메서드 추가

--- src/main/java/com/hotnerds/user/domain/repository/UserRepository.java
+++ src/main/java/com/hotnerds/user/domain/repository/UserRepository.java
@@ -3,5 +3,11 @@ package com.hotnerds.user.domain.repository;
 import com.hotnerds.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<User> findByUsername(String username);
+    List<User> findByEmail(String username);
+
 }
