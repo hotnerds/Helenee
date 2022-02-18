07ee0c8c20f3ea836fee63c7baf905e6e7dd7479
RIVERKIM <garamkim1357@gmail.com>
2022-02-18 20:31:33 +0900

    refactor: [Post] 기본 생성자 추가.

--- src/main/java/com/hotnerds/post/domain/Post.java
+++ src/main/java/com/hotnerds/post/domain/Post.java
@@ -1,10 +1,7 @@
 package com.hotnerds.post.domain;
 
 import com.hotnerds.user.domain.User;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.mapping.Join;
 
 import javax.persistence.*;
@@ -13,6 +10,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Post {
 
     @Id
