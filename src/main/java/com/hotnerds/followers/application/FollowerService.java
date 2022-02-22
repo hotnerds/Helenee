8316677d7e40fc9fdd5b7c31d27f3fcbe2ea4c6a
RetepMil <lkslyj2@naver.com>
2022-02-22 18:24:24 +0900

    feature: 팔로워 repo, service 생성

---
+++ src/main/java/com/hotnerds/followers/application/FollowerService.java
@@ -0,0 +1,9 @@
+package com.hotnerds.followers.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FollowerService {
+}
---
+++ src/main/java/com/hotnerds/followers/domain/repository/FollowerRepository.java
@@ -0,0 +1,8 @@
+package com.hotnerds.followers.domain.repository;
+
+import com.hotnerds.followers.domain.Follower;
+import com.hotnerds.followers.domain.FollowerFollowingId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FollowerRepository extends JpaRepository<Follower, FollowerFollowingId> {
+}
