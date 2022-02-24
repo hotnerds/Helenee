09f595c57ad02e283c0cab6d9d378674f04c7718
jungin0507 <a01033046758@gmail.com>
2022-02-25 02:19:20 +0900

    feature: 식단 Repository 생성

--- src/main/java/com/hotnerds/diet/domain/repoistory/DietRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hotnerds.diet.domain.repoistory;
-
-import com.hotnerds.diet.domain.Diet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DietRepository extends JpaRepository<Diet, Long> {
-}
