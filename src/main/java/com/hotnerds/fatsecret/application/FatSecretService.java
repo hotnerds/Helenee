83ffda3e44bc333d61a4e850e17e62d7adb486b5
Sangyups <feels4637@gmail.com>
2022-02-25 23:24:49 +0900

    feature: id를 통해 fatsecret에서 음식의 상세 정보 가져오는 기능 추가

---
+++ src/main/java/com/hotnerds/fatsecret/application/FatSecretService.java
@@ -0,0 +1,68 @@
+package com.hotnerds.fatsecret.application;
+
+import com.hotnerds.common.FatSecretConfig;
+import com.hotnerds.fatsecret.domain.dto.FatSecretFood;
+import com.hotnerds.fatsecret.domain.dto.FatSecretResponseDto;
+import com.hotnerds.fatsecret.domain.dto.FoodWrapper;
+import java.net.URI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
+public class FatSecretService {
+
+    private final String API_URI_PREFIX = "https://platform.fatsecret.com/rest/server.api";
+
+    private final RestTemplate restTemplate;
+
+    private final FatSecretConfig fatSecretConfig;
+
+    @Autowired
+    public FatSecretService(RestTemplateBuilder restTemplateBuilder,
+        FatSecretConfig fatSecretConfig) {
+        this.restTemplate = restTemplateBuilder.build();
+        this.fatSecretConfig = fatSecretConfig;
+    }
+
+    public FatSecretResponseDto getFoodById(Long foodId) {
+        final String METHOD = "food.get.v2";
+        final String FOOD_ID = foodId.toString();
+        final String FORMAT = "json";
+
+        final String token = fatSecretConfig.getToken();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(token);
+        HttpEntity httpEntity = new HttpEntity(headers);
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+
+        params.add("method", METHOD);
+        params.add("food_id", FOOD_ID);
+        params.add("format", FORMAT);
+
+        URI uri = UriComponentsBuilder
+            .fromHttpUrl(API_URI_PREFIX)
+            .queryParams(params)
+            .build()
+            .toUri();
+
+        ResponseEntity<FoodWrapper> responseEntity = restTemplate.exchange(uri,
+            HttpMethod.POST, httpEntity, FoodWrapper.class);
+
+        FatSecretFood fatSecretFood = responseEntity.getBody().getFatSecretFood();
+
+        return FatSecretResponseDto.of(fatSecretFood);
+    }
+
+}
