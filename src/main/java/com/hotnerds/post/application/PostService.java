831f3bfdddc78a259f01310ad448d25373beb2a4
RIVERKIM <garamkim1357@gmail.com>
2022-02-26 22:49:08 +0900

    feature: [PostService] 게시물 제목으로 조회 메서드 작성.

--- src/main/java/com/hotnerds/post/application/PostService.java
+++ src/main/java/com/hotnerds/post/application/PostService.java
@@ -2,15 +2,19 @@ package com.hotnerds.post.application;
 
 import com.hotnerds.post.domain.Post;
 import com.hotnerds.post.domain.dto.PostRequestDto;
+import com.hotnerds.post.domain.dto.PostResponseDto;
 import com.hotnerds.post.domain.repository.PostRepository;
 import com.hotnerds.user.domain.User;
 import com.hotnerds.user.domain.repository.UserRepository;
 import com.hotnerds.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +26,15 @@ public class PostService {
 
     public void write(PostRequestDto requestDto) {
         Post savedPost = postRepository.save(createPost(requestDto));
+    }
+
+    public List<PostResponseDto> searchByTitle(String title) {
+        List<Post> findPosts = postRepository.findAllByTitle(title);
+
+        return findPosts.stream()
+                .map(PostResponseDto::of)
+                .collect(Collectors.toList());
+
     }
 
     private Post createPost(PostRequestDto postRequestDto) {
