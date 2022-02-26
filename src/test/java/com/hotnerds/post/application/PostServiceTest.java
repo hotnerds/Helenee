2c56202b53d9ab5e107c907de0c54e1c6e390905
RIVERKIM <garamkim1357@gmail.com>
2022-02-26 22:48:08 +0900

    feature: [PostServiceTest] 게시물 제목으로 조회 테스트 작성.

--- src/test/java/com/hotnerds/post/application/PostServiceTest.java
+++ src/test/java/com/hotnerds/post/application/PostServiceTest.java
@@ -2,6 +2,7 @@ package com.hotnerds.post.application;
 
 import com.hotnerds.post.domain.Post;
 import com.hotnerds.post.domain.dto.PostRequestDto;
+import com.hotnerds.post.domain.dto.PostResponseDto;
 import com.hotnerds.post.domain.repository.PostRepository;
 import com.hotnerds.user.domain.User;
 import com.hotnerds.user.domain.repository.UserRepository;
@@ -11,9 +12,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -70,7 +74,31 @@ public class PostServiceTest {
         verify(postRepository, times(1)).save(any(Post.class));
     }
 
+    @DisplayName("게시글 제목으로 조회")
+    @Test
+    void 게시글_제목으로_조회() {
+        //given
+        User user = User.builder()
+                .username("name")
+                .email("email")
+                .build();
 
+        Post post = Post.builder()
+                .id(1L)
+                .title("title")
+                .content("content")
+                .writer(user)
+                .build();
+
+        when(postRepository.findAllByTitle(anyString())).thenReturn(List.of(post));
+        //when
+        List<PostResponseDto> findPosts = postService.searchByTitle(post.getTitle());
+
+        //then
+        assertThat(findPosts.size()).isEqualTo(1);
+
+        verify(postRepository, times(1)).findAllByTitle(post.getTitle());
+    }
 
 
 
