14139a588a56e6038e713a0bd36655c92da3f3a9
jungin0507 <a01033046758@gmail.com>
2022-03-07 03:15:35 +0900

    feat: [FatSecretResponseErrorHandler] FatSecret API 호출 에러 검출을 위한 ResponseErrorHandler 객체 구현

---
+++ src/main/java/com/hotnerds/fatsecret/exception/FatSecretResponseErrorHandler.java
@@ -0,0 +1,57 @@
+package com.hotnerds.fatsecret.exception;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+
+import javax.swing.text.html.Option;
+import javax.validation.constraints.Null;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Optional;
+
+import static java.util.Optional.ofNullable;
+
+@Component
+public class FatSecretResponseErrorHandler extends DefaultResponseErrorHandler {
+
+    public FatSecretResponseErrorHandler() {
+    }
+
+    @Override
+    public boolean hasError(ClientHttpResponse response) throws IOException {
+        return super.hasError(response) || hasError(getResponseBody(response));
+    }
+
+    @Override
+    public void handleError(ClientHttpResponse response) throws IOException {
+        Optional<HttpStatus> optionalStatusCode = Optional.ofNullable(HttpStatus.resolve(response.getRawStatusCode()))
+                .filter(HttpStatus::is2xxSuccessful);
+
+        if(optionalStatusCode.isPresent()) {
+            handleError(getResponseBody(response));
+        } else {
+            super.handleError(response);
+        }
+    }
+
+    protected boolean hasError(byte[] responseBody) throws IOException {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> responseEntity = objectMapper.readValue(responseBody, Map.class);
+        return responseEntity.containsKey("error");
+    }
+
+    protected void handleError(byte[] responseBody) throws IOException {
+        final ObjectMapper objectMapper = new ObjectMapper()
+                .enable(DeserializationFeature.UNWRAP_ROOT_VALUE); // @JsonRootName annotation 사용을 위한 설정
+
+        FatSecretResponseError error = objectMapper.readValue(responseBody, FatSecretResponseError.class);
+
+        throw new FatSecretResponseErrorException(error.getMessage());
+    }
+}
