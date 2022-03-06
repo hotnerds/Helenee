adbe88622c5a41d85dc880c0e6772f96121b4941
jungin0507 <a01033046758@gmail.com>
2022-03-07 02:06:51 +0900

    feat: [CustomResponseErrorException] FatSecret API 호출 에러시 반환될 Exception 클래스 추가

---
+++ src/main/java/com/hotnerds/fatsecret/exception/FatSecretResponseErrorException.java
@@ -0,0 +1,8 @@
+package com.hotnerds.fatsecret.exception;
+
+public class FatSecretResponseErrorException extends RuntimeException {
+
+    public FatSecretResponseErrorException(String message) {
+        super(message);
+    }
+}
