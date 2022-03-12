3c409a0a49d0892b1713250c3cac06ee90a96b53
RIVERKIM <garamkim1357@gmail.com>
2022-03-12 11:02:01 +0900

    충돌한 부분 다시 해결.

--- src/test/java/com/hotnerds/user/application/UserServiceTest.java
+++ src/test/java/com/hotnerds/user/application/UserServiceTest.java
@@ -1,10 +1,17 @@
 package com.hotnerds.user.application;
 
+import com.hotnerds.user.domain.Dto.FollowServiceReqDto;
 import com.hotnerds.user.domain.Dto.NewUserReqDto;
 import com.hotnerds.user.domain.Dto.UserUpdateReqDto;
+import com.hotnerds.user.domain.Follow;
+import com.hotnerds.user.domain.FollowedList;
+import com.hotnerds.user.domain.FollowerList;
 import com.hotnerds.user.domain.User;
 import com.hotnerds.user.domain.repository.UserRepository;
-import org.junit.jupiter.api.Assertions;
+import com.hotnerds.user.exception.FollowRelationshipExistsException;
+import com.hotnerds.user.exception.FollowRelationshipNotFound;
+import com.hotnerds.user.exception.UserNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,8 +24,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -40,6 +48,34 @@ class UserServiceTest {
                     .build()
     );
 
+    private User user1;
+    private User user2;
+    private Follow follow;
+    private FollowServiceReqDto reqDto;
+
+    @BeforeEach
+    void setUp() {
+        user1 = User.builder()
+                .username("user1")
+                .email("user1@gmail.com")
+                .build();
+
+        user2 = User.builder()
+                .username("user2")
+                .email("user2@gmail.com")
+                .build();
+
+        reqDto = FollowServiceReqDto.builder()
+                .followerId(1L) // given id for user1
+                .followedId(2L) // given id for user2
+                .build();
+
+        follow = Follow.builder()
+                .follower(user1)
+                .followed(user2)
+                .build();
+    }
+
     @Test
     void createNewUser() {
         // mocking
@@ -59,17 +95,17 @@ class UserServiceTest {
     void getAllUsers() {
         // mocking
         when(userRepository.findAll()).thenReturn(actualNewUserReqDtoList.stream()
-                .map(newUserDto -> newUserDto.toEntity())
+                .map(NewUserReqDto::toEntity)
                 .collect(Collectors.toList()));
 
         // when
         List<User> gotUserList = userService.getAllUsers();
 
         // then
-        Assertions.assertEquals(2, gotUserList.size());
-        Assertions.assertTrue(actualNewUserReqDtoList.get(0).toEntity()
+        assertEquals(2, gotUserList.size());
+        assertTrue(actualNewUserReqDtoList.get(0).toEntity()
                 .equals(gotUserList.get(0)));
-        Assertions.assertTrue(actualNewUserReqDtoList.get(1).toEntity()
+        assertTrue(actualNewUserReqDtoList.get(1).toEntity()
                 .equals(gotUserList.get(1)));
     }
 
@@ -83,7 +119,7 @@ class UserServiceTest {
         User userFound = userService.getUserById(user.getId());
 
         // then
-        Assertions.assertEquals(user, userFound);
+        assertEquals(user, userFound);
     }
 
     @Test
@@ -101,8 +137,263 @@ class UserServiceTest {
         userService.updateUser(user.getId(), userUpdateReqDto);
 
         // then
-        // 반환이 void인 createNewUser, deleteUserById, updateUser 같은 경우
-        // DB에 적용이 되는지 확인하는 절차가 필요하기 때문에 테스트가 애매함
-        // 피드백 필요
     }
+
+    @Test
+    @DisplayName("새로운 팔로우 관계를 생성할때 같은 관계가 존재하면 예외 발생")
+    void 팔로우_중복_확인() {
+        // given
+        user1.getFollowedList().add(follow);
+        user2.getFollowerList().add(follow);
+
+        when(userRepository.findById(reqDto.getFollowerId())).thenReturn(Optional.of(user1));
+        when(userRepository.findById(reqDto.getFollowedId())).thenReturn(Optional.of(user2));
+
+        // when then
+        assertThrows(FollowRelationshipExistsException.class, () -> userService.createFollow(reqDto));
+        verify(userRepository, times(1)).findById(reqDto.getFollowerId());
+        verify(userRepository, times(1)).findById(reqDto.getFollowedId());
+    }
+
+    @Test
+    @DisplayName("새로운 팔로우 관계를 생성할 수 있다")
+    void 새로운_팔로우_관계_생성() {
+        // given
+        when(userRepository.findById(reqDto.getFollowerId())).thenReturn(Optional.of(user1));
+        when(userRepository.findById(reqDto.getFollowedId())).thenReturn(Optional.of(user2));
+
+        // when
+        Follow response = userService.createFollow(reqDto);
+
+        // then
+        assertTrue(follow.getFollower() == response.getFollower() &&
+                follow.getFollowed() == response.getFollowed());
+        verify(userRepository, times(1)).findById(reqDto.getFollowerId());
+        verify(userRepository, times(1)).findById(reqDto.getFollowedId());
+    }
+
+    @Test
+    @DisplayName("없는 정보의 팔로우 검색 시도 시 예외발생")
+    public void 없는_팔로우_관계_검색() {
+        // given
+        when(userRepository.findById(reqDto.getFollowerId())).thenReturn(Optional.of(user1));
+        when(userRepository.findById(reqDto.getFollowedId())).thenReturn(Optional.of(user2));
+
+        // when then
+        assertThrows(FollowRelationshipNotFound.class, () -> userService.getOneFollow(reqDto));
+        verify(userRepository, times(1)).findById(reqDto.getFollowerId());
+        verify(userRepository, times(1)).findById(reqDto.getFollowedId());
+    }
+
+    @Test
+    @DisplayName("팔로우 관계를 팔로워 ID와 피팔로워 ID로 검색 가능")
+    public void 팔로우_관계_검색() {
+        // given
+        user1.getFollowedList().add(follow);
+        user2.getFollowerList().add(follow);
+
+        when(userRepository.findById(reqDto.getFollowerId())).thenReturn(Optional.of(user1));
+        when(userRepository.findById(reqDto.getFollowedId())).thenReturn(Optional.of(user2));
+
+        // when
+        Follow searchResult = userService.getOneFollow(reqDto);
+
+        // then
+        assertEquals(follow.getFollower(), searchResult.getFollower());
+        assertEquals(follow.getFollowed(), searchResult.getFollowed());
+        verify(userRepository, times(1)).findById(reqDto.getFollowerId());
+        verify(userRepository, times(1)).findById(reqDto.getFollowedId());
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 id를 가진 유저를 팔로잉하는 모든 유저들의 id를 검색하면 예외발생")
+    public void 유저_팔로워_리스트_오류() {
+        // when then
+        assertThrows(UserNotFoundException.class, () -> userService.getUserFollowers(999L));
+    }
+
+    @Test
+    @DisplayName("특정 id의 유저를 팔로잉하는 모든 유저들의 id를 검색 가능")
+    public void 유저_팔로워_리스트_검색() {
+        // given
+        User mockedUser1 = mock(User.class);
+        User mockedUser2 = mock(User.class);
+        User mockedUser3 = mock(User.class);
+        FollowerList mockedList = mock(FollowerList.class);
+
+        follow = Follow.builder()
+                .follower(mockedUser1)
+                .followed(mockedUser2)
+                .build();
+
+        Follow anotherFollow = Follow.builder()
+                .follower(mockedUser3)
+                .followed(mockedUser2)
+                .build();
+
+        when(userRepository.findById(2L)).thenReturn(Optional.of(mockedUser2));
+        when(mockedUser1.getId()).thenReturn(1L);
+        when(mockedUser3.getId()).thenReturn(3L);
+        when(mockedUser2.getFollowerList()).thenReturn(mockedList);
+        when(mockedList.getFollowers()).thenReturn(Arrays.asList(follow, anotherFollow));
+
+        List<Long> expectedList = Arrays.asList(1L, 3L);
+
+        // when
+        List<Long> userIdList = userService.getUserFollowers(2L); // 2 is user id for user2
+
+        // then
+        assertEquals(expectedList.get(0), userIdList.get(0));
+        assertEquals(expectedList.get(1), userIdList.get(1));
+        verify(userRepository).findById(2L);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 id를 가진 유저가 팔로잉하는 모든 유저들의 id를 검색하면 예외발생")
+    public void 유저_팔로잉_리스트_오류() {
+        // when then
+        assertThrows(UserNotFoundException.class, () -> userService.getUserFollowings(999L));
+    }
+
+    @Test
+    @DisplayName("특정 id의 유저가 팔로잉하는 유저들의 모든 id를 검색 가능")
+    public void 유저_팔로잉_리스트_검색() {
+        // given
+        User mockedUser1 = mock(User.class);
+        User mockedUser2 = mock(User.class);
+        User mockedUser3 = mock(User.class);
+        FollowedList mockedList = mock(FollowedList.class);
+
+        follow = Follow.builder()
+                .follower(mockedUser2)
+                .followed(mockedUser1)
+                .build();
+
+        Follow anotherFollow = Follow.builder()
+                .follower(mockedUser2)
+                .followed(mockedUser3)
+                .build();
+
+        when(userRepository.findById(2L)).thenReturn(Optional.of(mockedUser2));
+        when(mockedUser1.getId()).thenReturn(1L);
+        when(mockedUser3.getId()).thenReturn(3L);
+        when(mockedUser2.getFollowedList()).thenReturn(mockedList);
+        when(mockedList.getFollowed()).thenReturn(Arrays.asList(follow, anotherFollow));
+
+        List<Long> expectedList = Arrays.asList(1L, 3L);
+
+        // when
+        List<Long> userIdList = userService.getUserFollowings(2L); // 2 is user id for user2
+
+        // then
+        assertEquals(expectedList.get(0), userIdList.get(0));
+        assertEquals(expectedList.get(1), userIdList.get(1));
+        verify(userRepository).findById(2L);
+    }
+
+    @Test
+    @DisplayName("특정 id를 가진 유저의 현재 팔로워 수 응답 가능")
+    public void 유저_팔로워_수() {
+        // given
+        User user3 = User.builder()
+                .username("user3")
+                .email("user3@gmail.com")
+                .build();
+
+        Follow anotherFollow = Follow.builder()
+                .follower(user3)
+                .followed(user2)
+                .build();
+
+        user1.getFollowedList().getFollowed().add(follow);
+        user2.getFollowerList().getFollowers().add(follow);
+        user3.getFollowedList().getFollowed().add(anotherFollow);
+        user2.getFollowerList().getFollowers().add(anotherFollow);
+
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(user2));
+
+        // when
+        Integer count = userService.getFollowerCounts(2L);
+
+        // then
+        assertEquals(2, count);
+        verify(userRepository).findById(anyLong());
+    }
+
+    @Test
+    @DisplayName("특정 id를 가진 유저의 현재 팔로워 수 응답 가능")
+    public void 유저_팔로잉_수() {
+        User user3 = User.builder()
+                .username("user3")
+                .email("user3@gmail.com")
+                .build();
+
+        Follow anotherFollow = Follow.builder()
+                .follower(user3)
+                .followed(user2)
+                .build();
+
+        user1.getFollowedList().getFollowed().add(follow);
+        user2.getFollowerList().getFollowers().add(follow);
+        user3.getFollowedList().getFollowed().add(anotherFollow);
+        user2.getFollowerList().getFollowers().add(anotherFollow);
+
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(user2));
+
+        // when
+        Integer count = userService.getFollowCounts(2L);
+
+        // then
+        assertEquals(0, count);
+        verify(userRepository).findById(anyLong());
+    }
+
+    @Test
+    @DisplayName("어떤 유저가 다른 유저를 팔로우하고 있는지 확인 가능")
+    public void 유저_팔로우_확인() {
+        // given
+        user1.getFollowedList().getFollowed().add(follow);
+        user2.getFollowerList().getFollowers().add(follow);
+
+        // when
+        boolean expectTrue = userService.isFollowExist(user1, user2);
+
+        // then
+        assertTrue(expectTrue);
+    }
+
+    @Test
+    @DisplayName("서로 팔로우 되어있는지 확인하는 기능")
+    public void 유저_뮤추얼_팔로우_확인() {
+        User user3 = User.builder()
+                .username("user3")
+                .email("user3@gmail.com")
+                .build();
+
+        Follow mutualFollow = Follow.builder()
+                .follower(user2)
+                .followed(user1)
+                .build();
+
+        Follow anotherFollow = Follow.builder()
+                .follower(user3)
+                .followed(user2)
+                .build();
+
+        user1.getFollowedList().getFollowed().add(follow);
+        user2.getFollowerList().getFollowers().add(follow);
+        user2.getFollowedList().getFollowed().add(mutualFollow);
+        user1.getFollowerList().getFollowers().add(mutualFollow);
+        user3.getFollowedList().getFollowed().add(anotherFollow);
+        user2.getFollowerList().getFollowers().add(anotherFollow);
+
+        // when
+        boolean expectMutualTrue = userService.isMutualFollowExist(user1, user2);
+        boolean expectMutualFalse = userService.isMutualFollowExist(user2, user3); // user 3 follows user 2, but not vice versa
+
+        // then
+        assertTrue(expectMutualTrue);
+        assertFalse(expectMutualFalse);
+    }
+
 }
