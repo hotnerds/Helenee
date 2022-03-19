e73f8e3677386b1bcbcb964e67f4094411ae0973
jungin0507 <a01033046758@gmail.com>
2022-03-19 20:26:16 +0900

    feat: [DietService] 식단 정보 불러오는 기능 추가

--- src/main/java/com/hotnerds/diet/application/DietService.java
+++ src/main/java/com/hotnerds/diet/application/DietService.java
@@ -1,6 +1,14 @@
 package com.hotnerds.diet.application;
 
 
+import com.hotnerds.diet.domain.Diet;
+import com.hotnerds.diet.domain.dto.DietReadRequestDto;
+import com.hotnerds.diet.domain.dto.DietResponseDto;
+import com.hotnerds.diet.domain.repository.DietRepository;
+import com.hotnerds.diet.exception.DietNotFoundException;
+import com.hotnerds.user.domain.User;
+import com.hotnerds.user.domain.repository.UserRepository;
+import com.hotnerds.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,5 +18,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class DietService {
 
+    private final UserRepository userRepository;
+    private final DietRepository dietRepository;
 
+    public DietResponseDto findByDateTimeUser(DietReadRequestDto requestDto, Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(UserNotFoundException::new);
+
+        Diet diet = dietRepository.findByDateTimeUser(requestDto.getMealDate(), requestDto.getMealTime(), user)
+                .orElseThrow(DietNotFoundException::new);
+
+        return DietResponseDto.of(diet);
+    }
 }
