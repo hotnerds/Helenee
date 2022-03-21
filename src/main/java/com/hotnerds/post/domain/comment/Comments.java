3b963682dead52d21afde675c04f0bdfc4bc5c5d
RetepMil <lkslyj2@naver.com>
2022-03-21 14:12:59 +0900

    feature: [Comments] 댓글 엔티티의 DDD 패턴 적용을 위한 일급 컬렉션 작성

---
+++ src/main/java/com/hotnerds/post/domain/comment/Comments.java
@@ -0,0 +1,26 @@
+package com.hotnerds.post.domain.comment;
+
+import lombok.Getter;
+
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class Comments {
+    @OneToMany(
+            mappedBy = "post",
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.PERSIST,
+            orphanRemoval = true
+    )
+    private List<Comment> comments = new ArrayList<>();
+
+    public Comment addComment(Comment comment) {
+        comments.add(comment);
+        return comment;
+    }
+
+}
