e11da8a986cea473d2ea6cb513d360953eaae5f6
RIVERKIM <garamkim1357@gmail.com>
2022-03-23 22:08:18 +0900

    feature: [Post] PostTags 및 Tag 관련된 기능 추가.

--- src/main/java/com/hotnerds/post/domain/Post.java
+++ src/main/java/com/hotnerds/post/domain/Post.java
@@ -3,17 +3,17 @@ package com.hotnerds.post.domain;
 import com.hotnerds.common.BaseTimeEntity;
 import com.hotnerds.post.domain.like.Like;
 import com.hotnerds.post.domain.like.Likes;
+import com.hotnerds.post.domain.tag.PostTags;
+import com.hotnerds.tag.domain.Tag;
 import com.hotnerds.user.domain.User;
 import lombok.*;
 import org.hibernate.mapping.Join;
 
 import javax.persistence.*;
 
-@Builder
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 public class Post extends BaseTimeEntity {
 
     @Id
@@ -33,6 +33,9 @@ public class Post extends BaseTimeEntity {
     @Embedded
     Likes likes;
 
+    @Embedded
+    PostTags postTags;
+
     public void like(User user) {
         Like like = Like.builder()
                 .id(null)
@@ -51,8 +54,33 @@ public class Post extends BaseTimeEntity {
         likes.remove(like);
     }
 
+    public void addTag(Tag tag) {
+        postTags.addTag(this, tag);
+    }
+
+    public void removeTag(Tag tag) {
+        postTags.removeTag(tag);
+    }
+
     public int getLikeCount() {
         return likes.getCount();
     }
 
+    public Post(String title, String content, User writer) {
+        this(null, title, content, writer, Likes.empty(), PostTags.empty());
+    }
+
+    public Post(Long id, String title, String content, User writer) {
+        this(id, title, content, writer, Likes.empty(), PostTags.empty());
+    }
+
+    @Builder
+    public Post(Long id, String title, String content, User writer, Likes likes, PostTags postTags) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+        this.likes = likes;
+        this.postTags = postTags;
+    }
 }
