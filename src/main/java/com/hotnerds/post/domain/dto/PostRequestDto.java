aca5510012fb1c993869dc6453aca84f890ae501
RIVERKIM <garamkim1357@gmail.com>
2022-03-23 22:10:20 +0900

    feature: [PostRequestDto] 게시글 생성 요청 시 태그 이름들 또한 받도록 DTO 수정.

--- src/main/java/com/hotnerds/post/domain/dto/PostRequestDto.java
+++ src/main/java/com/hotnerds/post/domain/dto/PostRequestDto.java
@@ -3,10 +3,13 @@ package com.hotnerds.post.domain.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @Builder
 public class PostRequestDto {
     private String title;
     private String content;
     private String username;
+    private List<String> tagNames;
 }
