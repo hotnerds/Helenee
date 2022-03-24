03a040323efac87ffe50435d16f5bcf6aa313178
RetepMil <lkslyj2@naver.com>
2022-03-24 13:31:01 +0900

    style: 필요없는 import 삭제

--- src/main/java/com/hotnerds/post/domain/repository/PostRepository.java
+++ src/main/java/com/hotnerds/post/domain/repository/PostRepository.java
@@ -2,14 +2,12 @@ package com.hotnerds.post.domain.repository;
 
 import com.hotnerds.post.domain.Post;
 import com.hotnerds.user.domain.User;
-import org.apache.tomcat.jni.Local;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
