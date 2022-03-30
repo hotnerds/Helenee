e0c919c6d627b2a1e2954f9d47f9d11284c92595
RetepMil <lkslyj2@naver.com>
2022-03-30 12:01:02 +0900

    fix: [CommentController] 반환 객체 변경 및 API URI 변경

--- src/main/java/com/hotnerds/comment/presentation/CommentController.java
+++ src/main/java/com/hotnerds/comment/presentation/CommentController.java
@@ -1,6 +1,7 @@
 package com.hotnerds.comment.presentation;
 
 import com.hotnerds.comment.application.CommentService;
+import com.hotnerds.comment.domain.Comment;
 import com.hotnerds.comment.domain.Dto.*;
 import com.hotnerds.common.security.oauth2.annotation.AuthenticatedUser;
 import com.hotnerds.user.domain.User;
@@ -17,34 +18,34 @@ import static com.hotnerds.comment.presentation.CommentController.DEFAULT_URL;
 @RequiredArgsConstructor
 @RequestMapping(DEFAULT_URL)
 public class CommentController {
-    public static final String DEFAULT_URL = "/comment";
+    public static final String DEFAULT_URL = "/api/posts/{post_id}/comments";
     private final CommentService commentService;
 
     @PostMapping
-    public ResponseEntity<Void> createComment(@RequestBody CommentCreateReqDto reqDto) {
-        commentService.addComment(reqDto);
-
-        return ResponseEntity.ok().build();
+    public ResponseEntity<?> createComment(@RequestBody CommentCreateReqDto reqDto) {
+        Comment newComment = commentService.addComment(reqDto);
+        CommentResponseDto responseDto = CommentResponseDto.Of(newComment);
+        return ResponseEntity.ok(responseDto);
     }
 
     @DeleteMapping("/{comment_id}")
-    public ResponseEntity<Void> deleteComment(@RequestBody CommentDeleteReqDto reqDto, @AuthenticatedUser User user) {
+    public ResponseEntity<?> deleteComment(@PathVariable("post_id") Long post_id, @PathVariable("comment_id") Long comment_id, @AuthenticatedUser User user) {
+        CommentDeleteReqDto reqDto = new CommentDeleteReqDto(post_id, comment_id);
         commentService.deleteComment(reqDto, user.getId());
-
-        return ResponseEntity.accepted().build();
+        return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("/{comment_id}")
-    public ResponseEntity<Void> updateComment(@RequestBody CommentUpdateReqDto reqDto, @AuthenticatedUser User user) {
-        commentService.updateComment(reqDto, user.getId());
-
-        return ResponseEntity.accepted().build();
+    public ResponseEntity<?> updateComment(@RequestBody CommentUpdateReqDto reqDto, @AuthenticatedUser User user) {
+        Comment updatedComment = commentService.updateComment(reqDto, user.getId());
+        CommentResponseDto responseDto = CommentResponseDto.Of(updatedComment);
+        return ResponseEntity.ok(responseDto);
     }
 
     @GetMapping
-    public ResponseEntity<List<CommentResponseDto>> getComment(@RequestParam Long postid, @RequestParam Integer page, @RequestParam Integer size) {
+    public ResponseEntity<List<CommentResponseDto>> getComment(@PathVariable("post_id") Long post_id, @RequestParam Integer page, @RequestParam Integer size) {
         return ResponseEntity.ok(commentService.getComments(CommentByPostReqDto.builder()
-                .postId(postid)
+                .postId(post_id)
                 .pageable(PageRequest.of(page, size))
                 .build()));
     }
