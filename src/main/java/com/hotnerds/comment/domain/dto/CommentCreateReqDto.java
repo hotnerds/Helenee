2cf557346f33e92c74ae2d56f57dd123680267d3
jungin0507 <a01033046758@gmail.com>
2022-04-07 17:26:27 +0900

    fix: Comment DTO 클래스에 누락된 기본 생성자 추가

--- src/main/java/com/hotnerds/comment/domain/dto/CommentCreateReqDto.java
+++ src/main/java/com/hotnerds/comment/domain/dto/CommentCreateReqDto.java
@@ -1,12 +1,21 @@
 package com.hotnerds.comment.domain.dto;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Builder
 @Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CommentCreateReqDto {
     private Long userId;
     private Long postId;
     private String content;
+
+    @Builder
+    public CommentCreateReqDto(Long userId, Long postId, String content) {
+        this.userId = userId;
+        this.postId = postId;
+        this.content = content;
+    }
 }
--- src/main/java/com/hotnerds/comment/domain/dto/CommentResponseDto.java
+++ src/main/java/com/hotnerds/comment/domain/dto/CommentResponseDto.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-@Builder
 @Getter
 public class CommentResponseDto {
     private Long commentId;
@@ -14,6 +13,15 @@ public class CommentResponseDto {
     private Long postId;
     private String content;
     private LocalDateTime createdAt;
+
+    @Builder
+    public CommentResponseDto(Long commentId, Long userId, Long postId, String content, LocalDateTime createdAt) {
+        this.commentId = commentId;
+        this.userId = userId;
+        this.postId = postId;
+        this.content = content;
+        this.createdAt = createdAt;
+    }
 
     public static CommentResponseDto of(Comment comment) {
         return CommentResponseDto.builder()
