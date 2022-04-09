070907df270b2658ec0dc3f648224c42604cd3ca
RIVERKIM <garamkim1357@gmail.com>
2022-04-09 13:39:55 +0900

    feature: /api/posts 전체 게시글 조회 api 작성.

---
+++ src/main/java/com/hotnerds/post/presentation/PostController.java
@@ -0,0 +1,36 @@
+package com.hotnerds.post.presentation;
+
+
+import com.hotnerds.post.application.PostService;
+import com.hotnerds.post.domain.dto.PostByTitleRequestDto;
+import com.hotnerds.post.domain.dto.PostByWriterRequestDto;
+import com.hotnerds.post.domain.dto.PostResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static com.hotnerds.post.presentation.PostController.*;
+
+@RestController
+@RequestMapping(POST_API_URI)
+@RequiredArgsConstructor
+public class PostController {
+    public static final String POST_API_URI = "/api/posts";
+
+    private final PostService postService;
+
+    @GetMapping
+    public ResponseEntity<List<PostResponseDto>> searchAllPosts(@PageableDefault(size = 5) Pageable pageable) {
+        return ResponseEntity.ok(postService.searchAll(pageable));
+    }
+
+
+
+}
