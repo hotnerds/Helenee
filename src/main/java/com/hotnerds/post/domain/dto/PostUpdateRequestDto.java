7e13db335c47e3adc76183e72f4140243cdb94a4
RIVERKIM <garamkim1357@gmail.com>
2022-04-09 20:51:12 +0900

    feature: 필드 검증 어노테이션 추가.

--- src/main/java/com/hotnerds/post/domain/dto/PostUpdateRequestDto.java
+++ src/main/java/com/hotnerds/post/domain/dto/PostUpdateRequestDto.java
@@ -3,14 +3,22 @@ package com.hotnerds.post.domain.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
 public class PostUpdateRequestDto {
+    @NotNull
     private Long postId;
+    @NotBlank
     private String title;
+    @NotBlank
     private String content;
-    private List<String> tagNames;
+
+    @NotEmpty
+    private List<@NotBlank String> tagNames;
 
     @Builder
     public PostUpdateRequestDto(Long postId, String title, String content, List<String> tagNames) {
