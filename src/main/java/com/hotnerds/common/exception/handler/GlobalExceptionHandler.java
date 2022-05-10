14730fe2a1aa9225d1f66e5c5fe7cf400d048e99
RIVERKIM <garamkim1357@gmail.com>
2022-05-10 19:34:39 +0900

    feat: 예외 처리에 logging 추가.

--- src/main/java/com/hotnerds/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/hotnerds/common/exception/handler/GlobalExceptionHandler.java
@@ -19,8 +19,11 @@ import javax.naming.AuthenticationException;
 @Slf4j
 public class GlobalExceptionHandler {
 
+    private static final String LOG_FORMAT = "Exception: {}, ErrorCode: {}, Message: {}";
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        log.info(LOG_FORMAT, e.getClass().getName(), ErrorCode.INVALID_INPUT_VALUE_EXCEPTION.getCode(), ErrorCode.INVALID_INPUT_VALUE_EXCEPTION.getMessage());
         final BindingResult bindingResult = e.getBindingResult();
         final ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE_EXCEPTION, bindingResult);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
@@ -28,12 +31,14 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException e) {
+        log.info(LOG_FORMAT, e.getClass().getName(), ErrorCode.METHOD_NOT_ALLOWED_EXCEPTION.getCode(), ErrorCode.METHOD_NOT_ALLOWED_EXCEPTION.getMessage());
         final ErrorResponse response = ErrorResponse.of(ErrorCode.METHOD_NOT_ALLOWED_EXCEPTION);
         return new ResponseEntity<>(response, HttpStatus.METHOD_NOT_ALLOWED);
     }
 
     @ExceptionHandler(AuthenticationException.class)
     protected ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException e) {
+        log.info(LOG_FORMAT, e.getClass().getName(), ErrorCode.AUTHENTICATION_EXCEPTION.getCode(), ErrorCode.AUTHENTICATION_EXCEPTION.getMessage());
         ErrorResponse response =  ErrorResponse.of(ErrorCode.AUTHENTICATION_EXCEPTION, e.getMessage());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
@@ -41,25 +46,29 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BusinessException.class)
     protected ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
         ErrorCode errorCode = e.getErrorCode();
+        log.info(LOG_FORMAT, e.getClass().getName(), errorCode.getCode(), errorCode.getMessage());
         ErrorResponse response = ErrorResponse.of(errorCode);
         return new ResponseEntity<>(response, HttpStatus.valueOf(response.getStatus()));
     }
 
     @ExceptionHandler(IllegalStateException.class)
     protected ResponseEntity<ErrorResponse> handleIllegalStateException(IllegalStateException e) {
-        ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR_EXCEPTION);
-        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        log.info(LOG_FORMAT, e.getClass().getName(), ErrorCode.INVALID_INPUT_VALUE_EXCEPTION.getCode(), ErrorCode.INVALID_INPUT_VALUE_EXCEPTION.getMessage());
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE_EXCEPTION);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(BindException.class)
     protected ResponseEntity<ErrorResponse> handleBindException(BindException e) {
+        log.info(LOG_FORMAT, e.getClass().getName(), ErrorCode.INVALID_TYPE_VALUE_EXCEPTION.getCode(), ErrorCode.INVALID_TYPE_VALUE_EXCEPTION.getMessage());
         BindingResult bindingResult = e.getBindingResult();
-        ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE_EXCEPTION, bindingResult);
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_TYPE_VALUE_EXCEPTION, bindingResult);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.info(LOG_FORMAT, e.getClass().getName(), ErrorCode.INTERNAL_SERVER_ERROR_EXCEPTION.getCode(), ErrorCode.INTERNAL_SERVER_ERROR_EXCEPTION.getMessage());
         ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR_EXCEPTION);
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
