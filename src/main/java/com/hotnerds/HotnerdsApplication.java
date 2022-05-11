a005b864c1bccf05bcec14f76c9380349a1a0896
RIVERKIM <34048638+RIVERKIM@users.noreply.github.com>
2022-05-11 21:27:34 +0900

    Revert "develop to main (#88)"
    This reverts commit 87f189d15f37e00d0207bb6c6313c4d4c66ad301.

--- src/main/java/com/hotnerds/HotnerdsApplication.java
+++ src/main/java/com/hotnerds/HotnerdsApplication.java
@@ -2,11 +2,8 @@ package com.hotnerds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class HotnerdsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/hotnerds/food/domain/repository/FoodRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hotnerds.food.domain.repository;
-
-import com.hotnerds.food.domain.Food;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FoodRepository extends JpaRepository<Food, Long> {
-
-}
